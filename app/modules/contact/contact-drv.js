a1ff30c020897a6e0057911e96bf512ca4b9667d
rsmith-finix <rsmith@finixcreative.com>
2016-04-07 17:34:27 -0700

    Footer - directive paths
    Update directive paths to use new footer elements

--- app/modules/contact/contact-drv.js
+++ app/modules/contact/contact-drv.js
@@ -2,7 +2,7 @@ angular.module('coreMod')
 	.directive('contact', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/web/app/modules/contact/contact.html',
+			templateUrl: 'app/modules/contact/contact.html',
 			controller: 'ClientController',
 			controllerAs: 'client',
 		};
