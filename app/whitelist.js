92aaa0c764675d31f1d848d994bac80d8bf349c2
rsmith-finix <rsmith@finixcreative.com>
2016-04-07 19:40:31 -0700

    Whitelist - google
    Add google to whitelist

---
+++ app/whitelist.js
@@ -0,0 +1,10 @@
+angular.module('finixApp')
+	.config(['$sceDelegateProvider', function($sceDelegateProvider){
+		$sceDelegateProvider.resourceUrlWhitelist([
+			// Allow same origin resource loads.
+			'self',
+			// Allow loading from our assets domain.  Notice the difference between * and **.
+			'http*://**.google.com/**'
+		]);
+	}])
+;
