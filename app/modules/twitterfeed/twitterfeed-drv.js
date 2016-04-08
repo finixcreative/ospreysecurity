26d16e643256bd93b3f4202f00960e25b0bd7e9e
rsmith-finix <rsmith@finixcreative.com>
2016-04-07 20:45:45 -0700

    Twittered - getfeed
    Move function into directive

--- app/modules/twitterfeed/twitterfeed-drv.js
+++ app/modules/twitterfeed/twitterfeed-drv.js
@@ -1,7 +1,20 @@
 angular.module('coreMod')
 	.directive('twitterfeed', function(){
+		function getFeed(d, s, id){
+			var js,
+				fjs = d.getElementsByTagName(s)[0],
+				p = /^http:/.test(d.location) ? 'http' : 'https';
+			if(!d.getElementById(id)){
+				js = d.createElement(s);
+				js.id = id;
+				js.src = p + "://platform.twitter.com/widgets.js";
+				fjs.parentNode.insertBefore(js,fjs);
+			}
+		}
+		getFeed(document, "script", "twitter-wjs");
 		return {
 			restrict: 'E',
 			templateUrl: 'app/modules/twitterfeed/twitterfeed.html'
 		};
-	});
+	})
+;
